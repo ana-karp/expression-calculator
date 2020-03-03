cf4b87182b4c08d5fc2865e0ef8c2f0329dd2a90
Anna <ana-karp@users.noreply.github.com>
2020-03-03 23:24:23 +0300

    fix: solution

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ const methods = {
     }
   };
   
-
-
+  function checkBrackets(str) {
+    let countOpen = str.split('').filter(el => el === '(').length;
+    let countClose = str.split('').filter(el => el === ')').length;
+    if (countOpen !== countClose) throw new Error("ExpressionError: Brackets must be paired");
+  }
+  
   function expressionCalculator(expr) {
-    if (expr.split('').filter(el => el === '(').length !== expr.split('').filter(el => el === ')').length) {
-      throw new Error("ExpressionError: Brackets must be paired");
-    }
-    expr = expr.split("").filter(el => el !== " ").join("");
-      
-      
+    checkBrackets(expr);
+    expr = expr
+      .split("")
+      .filter(el => el !== " ")
+      .join("");
     let exp = /\([^()]*\)/; // RegExp for expression in brackets
     while (expr.includes("(")) {
       expr = expr.replace(exp, withoutBracketsCalc);
@@ -33,14 +36,15 @@ const methods = {
     if (expr.includes("(")) {
       expr = expr.slice(1, -1);
     }
-    let exp1 = /\-?\d+\.?\d*\s*[*/]\s*\-?\d+\.?\d*/;
+    let exp1 = /\d+\.?\d*\s*[*/]\s*\-?\d+\.?\d*/;
     let exp2 = /\-?\d+\.?\d*\s*[+-]\s*\-?\d+\.?\d*/;
   
-    while (expr.includes("*") || expr.includes("/")) {
+    while (exp1.test(expr)) {
       expr = expr.replace(exp1, calcString);
     }
     while (exp2.test(expr)) {
       expr = expr.replace(exp2, calcString);
+    
     }
     return +expr;
   }
@@ -55,7 +59,6 @@ const methods = {
     return methods[op](a, b);
   }
   
-
 module.exports = {
     expressionCalculator
 }
